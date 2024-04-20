3f0e523b678b0cf456ae0f9fc3a7398a8d7b6fce
Samir <dev.sam693@gmail.com>
2024-04-20 19:00:29 +0530

    Types of Loop Added
    Signed-off-by: Samir <dev.sam693@gmail.com>

---
+++ loops.java
@@ -0,0 +1,75 @@
+package pkg1;
+
+public class loops {
+    public static void main(String[] args) {
+//-------------------------------------
+//        WHILE LOOP (NOTE: WHILE LOOP fist check condition if condition false then exit loop
+//                          or true then loop run unlit condition break otherwise loop run infinitely)
+//-------------------------------------
+//        int wl=0;
+//        while (wl<=10){
+//            System.out.println(wl);
+//            wl++;
+//        }
+
+//        int wl = 10;
+//        int wl1 = 0;
+//        while (wl1 <= 10) {
+//        while (wl >= 0) {
+//                System.out.println(" 10 - " + wl1 + " = " + wl);
+//                wl1++;
+//                wl--;
+//        }
+//        }
+
+//-------------------------------------
+//        DO WHILE LOOP (NOTE: DO WHILE LOOP fist ENTER the DO SECTION and RUN DO SECTION then cheque condition if condition
+//                             false then exit loop or true then loop run unlit condition break otherwise loop run infinitely)
+//-------------------------------------
+//        int wl=0;
+//        do{
+//            System.out.println(wl);
+//            wl++;
+//        }
+//        while (wl<=10);
+
+//        int wl=10;
+//        do{
+//            System.out.println(wl);
+//            wl++;
+//        }
+//        while (wl<=7);
+
+//        int wl = 6;
+//        int wl1 = 1;
+//        do {
+//            System.out.println(" 5 + " + wl1 + " = " + wl);
+//            wl1++;
+//            wl++;
+//        }
+//        while (wl1 < 51);
+
+//--------------------------------------
+//        FOR LOOP (NOTE: FOR LOOP fist enter INITIALIZATION then check CONDITION and run to UP-TO-DATE SECTION
+//                        then run program AND again check CONDITION until CONDITION false then exit loop)
+//--------------------------------------
+//        for (int fl=1; fl<=10; fl++){
+//            System.out.println(" 5 * "+ fl+ " = "+ 5*fl);
+//        }
+        int fl1=10;
+            System.out.println("Here is first Odd 10 numbers");
+        for (int i=0; i<fl1; i++) {
+            System.out.println(2 * i + 1);
+        }
+        int fl2=10;
+            System.out.println("Here is first Odd 10 numbers");
+        for (int i=0; i<fl2; i++){
+            System.out.println(2*i);
+        }
+
+
+//--------------------------------------
+
+
+    }
+}
