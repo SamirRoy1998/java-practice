7fba5d42dee1066aac7fb199e7b229119d821b41
Samir <dev.sam693@gmail.com>
2024-04-22 13:22:12 +0530

    Static methods added
    Signed-off-by: Samir <dev.sam693@gmail.com>

---
+++ methodsInJava.java
@@ -0,0 +1,32 @@
+package pkg1;
+
+public class methodsInJava {
+//--------------------------------------------------------------------
+//          STATIC METHODS
+//--------------------------------------------------------------------
+    static int logic1 (int x, int y){
+        int z;
+        if (x>y){
+            z= x+y;
+        }
+        else {
+            z= x*y;
+        }
+        return z;
+    }
+//--------------------------------------------------------------------
+    public static void main(String[] args) {
+        int a1= 5;
+        int b1= 7;
+        int c1;
+        c1= logic1(a1,b1);      //-> Using STATIC METHODS
+
+        int a2= 9;
+        int b2= 7;
+        int c2;
+        c2= logic1(a2,b2);      //-> Using STATIC METHODS
+
+        System.out.println(c1);
+        System.out.println(c2);
+    }
+}
