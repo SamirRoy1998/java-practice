85714d1587712881a2cad2ec1cbd888d9506b222
Samir <dev.sam693@gmail.com>
2024-04-22 16:12:36 +0530

    VarAges added
    Signed-off-by: Samir <dev.sam693@gmail.com>

--- methodsInJava.java
+++ methodsInJava.java
@@ -28,7 +28,7 @@ public class methodsInJava {
         return z;
     }
 //--------------------------------------------------------------------
-//            METHODS OVERLOADING
+//          METHODS OVERLOADING
 //--------------------------------------------------------------------
     static void mo(){
         System.out.println("Hi! Nice to meet you.");
@@ -38,6 +38,16 @@ public class methodsInJava {
     }
     static void mo(String a,String b){
         System.out.println("Hi "+a+" and "+b+"! Nice to meet you both.");
+    }
+//--------------------------------------------------------------------
+//          VARARGS (Variable Arguments)
+//--------------------------------------------------------------------
+    static int sum(int ...arr){
+        int result=0;
+        for (int a:arr){
+            result += a;
+        }
+        return result;
     }
 //--------------------------------------------------------------------
     public static void main(String[] args) {
@@ -64,5 +74,13 @@ public class methodsInJava {
         mo();
         mo("Samir");
         mo("Samir","Susmita");
+        System.out.println("\n");
+
+        System.out.println("Welcome to VarAges.");
+        System.out.println("The sum of nothing is: " + sum());
+        System.out.println("The sum of 8 is: " + sum(8));
+        System.out.println("The sum of 2 and 3 is: " + sum(2,3));
+        System.out.println("The sum of 6,5 and 8 is: " + sum(6,5,8));
+        System.out.println("The sum of 14,18,30,10,15 and 19 is: " + sum(14,18,30,10,15,19));
     }
 }
