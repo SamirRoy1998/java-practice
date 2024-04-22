971d31e8efad65b62b4ac68f8220b860b68a5a54
Samir <dev.sam693@gmail.com>
2024-04-22 13:59:57 +0530

    Non-Static method added
    Signed-off-by: Samir <dev.sam693@gmail.com>

--- methodsInJava.java
+++ methodsInJava.java
@@ -2,7 +2,7 @@ package pkg1;
 
 public class methodsInJava {
 //--------------------------------------------------------------------
-//          STATIC METHODS
+//          STATIC METHODS (Note: Using static method you don't have to create object )
 //--------------------------------------------------------------------
     static int logic1 (int x, int y){
         int z;
@@ -14,19 +14,40 @@ public class methodsInJava {
         }
         return z;
     }
+
+//--------------------------------------------------------------------
+//          NON-STATIC METHODS (Note: Using non-static methods you have to create an object to call logic)
+//--------------------------------------------------------------------
+    int logic2 (int x, int y){
+        int z;
+        if (x>y){
+            z= x+y;
+        }
+        else {
+            z= x*y;
+        }
+        return z;
+    }
 //--------------------------------------------------------------------
     public static void main(String[] args) {
+        methodsInJava obj=new methodsInJava();  //-> Creating an object for using NON_STATIC METHODS
         int a1= 5;
         int b1= 7;
-        int c1;
-        c1= logic1(a1,b1);      //-> Calling using STATIC METHODS
+        int c1l1;
+        int c1l2;
+        c1l1= logic1(a1,b1);          //-> Calling logic using STATIC METHODS
+        c1l2= obj.logic2(a1,b1);      //-> Calling logic using NON-STATIC METHODS
 
         int a2= 9;
         int b2= 7;
-        int c2;
-        c2= logic1(a2,b2);      //-> Calling using STATIC METHODS
+        int c2l1;
+        int c2l2;
+        c2l1= logic1(a2,b2);          //-> Calling logic using STATIC METHODS
+        c2l2= obj.logic2(a2,b2);      //-> Calling logic using NON-STATIC METHODS
 
-        System.out.println(c1);
-        System.out.println(c2);
+        System.out.println(c1l1+" = This is 1st data's result using STATIC METHODS");
+        System.out.println(c1l2+" = This is 1st data's result using NON-STATIC METHODS");
+        System.out.println(c2l1+" = This is 2nd data's result using STATIC METHODS");
+        System.out.println(c2l2+" = This is 2nd data's result using NON-STATIC METHODS");
     }
 }
