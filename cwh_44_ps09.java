5cca28ee79cfb975a89d2128a9e645d948e62f6f
SamirRoy1998 <dev.sam693@gmail.com>
2024-04-29 02:40:38 +0530

    problem 2 solved
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- cwh_44_ps09.java
+++ cwh_44_ps09.java
@@ -19,17 +19,33 @@ class Cylinder1 {
     public void setRadius(int radius) {
         this.radius = radius;
     }
+
+    public double surfaceArea() {
+        return 2 * Math.PI * radius * radius + 2 * Math.PI * radius * height;
+    }
+
+    public double volume() {
+        return Math.PI * radius * radius * height;
+    }
 }
 
 public class cwh_44_ps09 {
-    public static void main(String[] args) {
-
-        //problem 1
+    public static void main() {
+        /*
+        problem 1
         Cylinder1 cylinder = new Cylinder1();
         cylinder.setHeight(24);
         cylinder.setRadius(6);
         System.out.println(cylinder.getHeight());
         System.out.println(cylinder.getRadius());
-
+        */
+        //problem 2
+        Cylinder1 cylinder = new Cylinder1();
+        cylinder.setHeight(24);
+        cylinder.setRadius(6);
+        System.out.println(cylinder.getHeight());
+        System.out.println(cylinder.getRadius());
+        System.out.println(cylinder.surfaceArea());
+        System.out.println(cylinder.volume());
     }
 }
