b7a011510a121e47ef75c88abec3c33a93a624ec
SamirRoy1998 <dev.sam693@gmail.com>
2024-04-30 21:17:24 +0530

    restart program not fix
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/RockPaperScissor.java
+++ Java_Practice/RockPaperScissor.java
@@ -4,18 +4,18 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissor {
-    public static void main(String[] args) {
+    public static void main() {
         Random rn = new Random();
         Scanner sc = new Scanner(System.in);
         System.out.println("1=Rock, 2=Paper, 3=Scissor");
 
         int myNum = sc.nextInt();
         int comNum = rn.nextInt(1, 4);
-        System.out.println("Your num: " + myNum);
-        System.out.println("System num: " + comNum);
+        boolean condition=(myNum > 0 & myNum < 4);
 
-        if (myNum > 0 & myNum < 4) {
-
+        if (condition) {
+            System.out.printf("Your num: %d%n", myNum);
+            System.out.printf("System num: %d%n", comNum);
             if (myNum == 1 & comNum == 2) {
                 System.out.println("System win");
             } else if (myNum == 2 & comNum == 3) {
@@ -31,6 +31,8 @@ public class RockPaperScissor {
             } else {
                 System.out.println("Draw");
             }
+        }else {
+            System.out.format("Incorrect move \nCtrl+F5 to restart");
         }
     }
 }
