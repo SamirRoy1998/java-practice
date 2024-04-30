e971fa80ffe9a570735dfc083c03465dc4421665
SamirRoy1998 <dev.sam693@gmail.com>
2024-04-30 20:37:17 +0530

    completed but one issue to fix
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/RockPaperScissor.java
+++ Java_Practice/RockPaperScissor.java
@@ -10,10 +10,27 @@ public class RockPaperScissor {
         System.out.println("1=Rock, 2=Paper, 3=Scissor");
 
         int myNum = sc.nextInt();
-        int comNum = rn.nextInt(1,4);
+        int comNum = rn.nextInt(1, 4);
+        System.out.println("Your num: " + myNum);
+        System.out.println("System num: " + comNum);
 
-        if (myNum==1& comNum==2){
-            System.out.println("");
+        if (myNum > 0 & myNum < 4) {
+
+            if (myNum == 1 & comNum == 2) {
+                System.out.println("System win");
+            } else if (myNum == 2 & comNum == 3) {
+                System.out.println("System win");
+            } else if (myNum == 3 & comNum == 1) {
+                System.out.println("System win");
+            } else if (myNum == 1 & comNum == 3) {
+                System.out.println("You win");
+            } else if (myNum == 3 & comNum == 2) {
+                System.out.println("You win");
+            } else if (myNum == 2 & comNum == 1) {
+                System.out.println("You win");
+            } else {
+                System.out.println("Draw");
+            }
         }
     }
 }
