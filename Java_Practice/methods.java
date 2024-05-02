12702e2120c2786c386b847aa47639487bd81719
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-03 01:02:20 +0530

    completed
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/methods.java
+++ Java_Practice/methods.java
@@ -1,4 +1,14 @@
 package pkg1.Java_Practice;
 
 public class methods {
+    int sum(int a, int b){
+        int c=a+b;
+        return c;
+    }
+    public static void main(String[] args) {
+        int x= 80;
+        int y=20;
+        methods obj=new methods();
+        obj.sum(x,y);
+    }
 }
