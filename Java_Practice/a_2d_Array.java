7242f690b540795f4132a756589a9aafb57ae3a6
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-02 22:31:50 +0530

    optimized
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/a_2d_Array.java
+++ Java_Practice/a_2d_Array.java
@@ -1,7 +1,7 @@
 package pkg1.Java_Practice;
 
 public class a_2d_Array {
-    public static void main(String[] args) {
+    public static void main() {
         int[][] flats = new int[2][3];
         flats[0][0] = 101;
         flats[0][1] = 102;
@@ -9,9 +9,9 @@ public class a_2d_Array {
         flats[1][0] = 201;
         flats[1][1] = 202;
         flats[1][2] = 203;
-        for (int i = 0; i < flats.length; i++) {
-            for (int j = 0; j < flats[i].length; j++) {
-                System.out.print(flats[i][j]);
+        for (int[] flat : flats) {
+            for (int i : flat) {
+                System.out.print(i);
                 System.out.print(" ");
             }
             System.out.println();
