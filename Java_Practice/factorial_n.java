f2dd263ee263c337235bb3a1465cd698028c29fc
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-03 20:30:01 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

---
+++ Java_Practice/factorial_n.java
@@ -0,0 +1,13 @@
+package pkg1.Java_Practice;
+
+public class factorial_n {
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
