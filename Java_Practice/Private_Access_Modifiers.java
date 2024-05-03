9b76e58bafe5b86b7728e1a2b33d6f6b7227f50a
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-03 22:29:33 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/Private_Access_Modifiers.java
+++ Java_Practice/Private_Access_Modifiers.java
@@ -1,7 +1,30 @@
 package pkg1.Java_Practice;
 
-public class Private_Access_Modifiers {
-    public static void main(String[] args) {
+class My_Employee {
+    private int id;
+    private String name;
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class Private_Access_Modifiers {
+    public static void main() {
+        My_Employee e1 = new My_Employee();
+        e1.setId(201);
+        e1.setName("Samir");
+        System.out.println(e1.getId());
+        System.out.println(e1.getName());
     }
 }
