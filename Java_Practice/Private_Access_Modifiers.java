bfe8785528e69edc115ffcfd23d13d72755ab85a
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-03 22:34:17 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/Private_Access_Modifiers.java
+++ Java_Practice/Private_Access_Modifiers.java
@@ -4,19 +4,11 @@ class My_Employee {
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+    public int getId() {return id;}
+    public void setId(int i) {id = i;}
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getName() {return name;}
+    public void setName(String n) {name = n;}
 }
 
 public class Private_Access_Modifiers {
