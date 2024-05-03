b3f6d0fa76e6aa4ed610ccf9240dd58bf1e468d4
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-03 15:54:34 +0530

    concept clear
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/Varargs.java
+++ Java_Practice/Varargs.java
@@ -1,4 +1,15 @@
 package pkg1.Java_Practice;
 
 public class Varargs {
+    static int sum(int... arr) {    //Available as (int[]arr)
+        int result = 0;
+        for (int e : arr) {
+            result += e;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(1,5,6,8));
+    }
 }
