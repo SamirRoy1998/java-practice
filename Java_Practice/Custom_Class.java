c4b17a10f831509207447c2e72b7cd7124d389c9
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-03 22:01:42 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/Custom_Class.java
+++ Java_Practice/Custom_Class.java
@@ -1,4 +1,30 @@
 package pkg1.Java_Practice;
 
+class Employee {
+    int id;
+    String name;
+    int salary;
+
+    public void getDetails() {
+        System.out.println("My name is: " + name + " and my Id is: " + id);
+    }
+
+    public int getSalary() {
+        System.out.println("My salary is: " + salary);
+        return salary;
+    }
+}
+
 public class Custom_Class {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+
+        e1.id = 249;
+        e1.name = "Samir";
+        e1.salary = 50000;
+
+        e1.getDetails();
+        e1.getSalary();
+
+    }
 }
