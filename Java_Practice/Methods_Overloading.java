ab15526054016138580c6e60216876111cfa3fd0
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-03 14:59:06 +0530

    concept clear
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/Methods_Overloading.java
+++ Java_Practice/Methods_Overloading.java
@@ -1,4 +1,32 @@
 package pkg1.Java_Practice;
 
 public class Methods_Overloading {
+    void greet() {
+        System.out.println("Hi! Nice to meet you.");
+    }
+
+    void greet(String a) {
+        System.out.println("Hi " + a + "! Nice to meet you.");
+    }
+
+    void greet(String a, String b) {
+        System.out.println("Hi " + a + " and " + b + "! Nice to meet you both.");
+    }
+
+    int greet(int a) {
+        System.out.println("Love you " + a);
+        return a;
+    }
+
+    public static void main(String[] args) {
+        String a = "Samir";
+        String b = "Susmita";
+        int num = 3000;
+
+        Methods_Overloading obj = new Methods_Overloading();
+        obj.greet();
+        obj.greet(a);
+        obj.greet(a, b);
+        obj.greet(num);
+    }
 }
