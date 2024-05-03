22d326b77477bfe1fc39d126dd53dcca6218c736
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-03 14:25:10 +0530

    modifying the array method added
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/methods.java
+++ Java_Practice/methods.java
@@ -5,10 +5,20 @@ public class methods {
         int c=a+b;
         return c;
     }
+    int modify(int[]arr){
+        arr[1]=75;
+        return arr[1];
+    }
     public static void main(String[] args) {
         int x= 80;
         int y=20;
         methods obj=new methods();
         obj.sum(x,y);
+
+        int [] marks={98,52,74,85,68};
+        obj.modify(marks);
+        for (int element:marks){
+            System.out.println(element);
+        }
     }
 }
