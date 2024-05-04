7ce30c8ba6ec4883ba13686462d9d62fae1d4057
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-04 12:15:06 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/Constructor_In_Inheritance.java
+++ Java_Practice/Constructor_In_Inheritance.java
@@ -1,4 +1,19 @@
 package pkg1.Java_Practice;
 
+class Parent {
+    public Parent() {System.out.println("I'm Parent");}
+}
+
+class Child extends Parent {
+    public Child() {System.out.println("I'm Child");}
+}
+
+class GrandChild extends Child {
+    public GrandChild() {System.out.println("I'm Grand Child");}
+}
+
 public class Constructor_In_Inheritance {
+    public static void main(String[] args) {
+        GrandChild gc = new GrandChild();
+    }
 }
