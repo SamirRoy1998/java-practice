eadb639552e73ee023256f2da4a27f902d658899
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-04 12:58:21 +0530

    super keyword
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/Constructor_In_Inheritance.java
+++ Java_Practice/Constructor_In_Inheritance.java
@@ -2,10 +2,13 @@ package pkg1.Java_Practice;
 
 class Parent {
     public Parent() {System.out.println("I'm Parent");}
+    public Parent(int i) {System.out.println("I'm Parent with value  of i is: "+i);}
 }
 
 class Child extends Parent {
-    public Child() {System.out.println("I'm Child");}
+    public Child() {
+        super(0);
+        System.out.println("I'm Child");}
 }
 
 class GrandChild extends Child {
@@ -14,6 +17,7 @@ class GrandChild extends Child {
 
 public class Constructor_In_Inheritance {
     public static void main(String[] args) {
-        GrandChild gc = new GrandChild();
+//        GrandChild gc = new GrandChild();
+        new Child();
     }
 }
