1e208c90a10c0a273bb91fb40eb33015396572a9
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-05 00:58:47 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Interfaces.java
+++ Interfaces.java
@@ -1,4 +1,38 @@
 package pkg1;
 
+interface bicycle {
+    void breaks(int decrement);
+
+    void accelerate(int increase);
+}
+
+interface horn {
+    void blowHorn();
+}
+
+class AvonCycle implements bicycle, horn {
+
+    @Override
+    public void breaks(int decrement) {
+        System.out.println("Breaks");
+    }
+
+    @Override
+    public void accelerate(int increase) {
+        System.out.println("Accelerate");
+    }
+
+    @Override
+    public void blowHorn() {
+        System.out.println("peep peep");
+    }
+}
+
 public class Interfaces {
+    public static void main(String[] args) {
+        AvonCycle ac = new AvonCycle();
+        ac.accelerate(10);
+        ac.breaks(2);
+        ac.blowHorn();
+    }
 }
