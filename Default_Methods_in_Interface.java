c76ceab38eb5167f98144f651930c5cd2c5b7a3b
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-05 15:50:45 +0530

    SmartPhone1 model ready
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Default_Methods_in_Interface.java
+++ Default_Methods_in_Interface.java
@@ -15,5 +15,30 @@ class CellPhone {
     void pickCall() {System.out.println("Connecting...");}
 }
 
+class SmartPhone1 extends CellPhone implements Camera,Wifi{
+
+    @Override
+    public void takePicture() {
+        System.out.println("Taking snap");
+    }
+
+    @Override
+    public void recordVideo() {
+        System.out.println("Recording..");
+    }
+
+    @Override
+    public String[] getNetwork() {
+        System.out.println("Getting the network list");
+        String[] networkList= {"Samir","Jio Fiber 826","Airtel X-Stream 23"};
+        return networkList;
+    }
+
+    @Override
+    public void ConnectToNetwork(String network) {
+        System.out.println("Connecting to "+ network);
+    }
+}
+
 public class Default_Methods_in_Interface {
 }
