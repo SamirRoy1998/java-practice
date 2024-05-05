b7878ba5119eb4b90d1621f7af0a3f3ab57179f3
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-05 15:39:32 +0530

    interfaces and structure ready
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Default_Methods_in_Interface.java
+++ Default_Methods_in_Interface.java
@@ -1,4 +1,19 @@
 package pkg1;
 
+interface Camera {
+    void takePicture();
+    void recordVideo();
+}
+
+interface Wifi {
+    String[] getNetwork();
+    void ConnectToNetwork(String network);
+}
+
+class CellPhone {
+    void callNumber(int phoneNumber) {System.out.printf("Calling %d%n", phoneNumber);}
+    void pickCall() {System.out.println("Connecting...");}
+}
+
 public class Default_Methods_in_Interface {
 }
