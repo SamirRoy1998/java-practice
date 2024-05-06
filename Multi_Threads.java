1d31444293302c40b48331ec49b059d6a8dc5f19
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-06 17:38:23 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Multi_Threads.java
+++ Multi_Threads.java
@@ -1,19 +1,21 @@
 package pkg1;
 
-class Example1 {
+class Example1 extends Thread {
+    @Override
     public void run() {
         int i = 0;
-        while (i < 50) {
+        while (i < 50000) {
             System.out.println("Example1 meth1");
             i++;
         }
     }
 }
 
-class Example2 {
+class Example2 extends Thread{
+    @Override
     public void run() {
         int i = 0;
-        while (i < 50) {
+        while (i < 50000) {
             System.out.println("Example2 meth1");
             i++;
         }
@@ -21,10 +23,15 @@ class Example2 {
 }
 
 public class Multi_Threads {
-    public static void main(String[] args) {
+    public static void main() {
         Example1 e1 = new Example1();
         Example2 e2 = new Example2();
-        e1.run();
-        e2.run();
+//        run without using threads
+//        e1.run(); --> run first then
+//        e2.run(); --> run this
+
+//        run using threads
+        e1.start();
+        e2.start();
     }
 }
