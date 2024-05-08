a7ee9de908c68e3ae4f58bec9ed99de0d461772c
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-09 01:33:51 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

---
+++ Nested_try_catch_With_while_loop.java
@@ -0,0 +1,29 @@
+package pkg1;
+
+import java.util.Scanner;
+
+public class Nested_try_catch_With_while_loop {
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50};
+        Scanner sc = new Scanner(System.in);
+        boolean flag=true;
+
+        while (flag){
+            System.out.print("Enter the index no: ");
+            int i = sc.nextInt();
+            try {
+                try {
+                    System.out.println(arr[i]);
+                    flag=false;
+                }
+                catch (ArrayIndexOutOfBoundsException e){
+                    System.out.println("Entered index number isn't available");
+                }
+            }
+            catch (Exception e){
+                System.out.println(e);
+            }
+        }
+        System.out.println("Thank you for using this program");
+    }
+}
