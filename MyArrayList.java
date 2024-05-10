a07022276504e57e93beea5aa3974985218c5d67
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-10 22:32:01 +0530

    some experiment
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- MyArrayList.java
+++ MyArrayList.java
@@ -15,9 +15,35 @@ public class MyArrayList {
         l1.add(84);
         l1.add(94);
         l1.add(91);
+        l1.add(0,10000);
 
-        for (int i=0; i< l1.size(); i++){
-            System.out.println(l1.get(i));
-        }
+//        for (int i=0; i< l1.size(); i++){
+//            System.out.println(l1.get(i));
+//        }
+        System.out.println(l1);
+
+
+        ArrayList<String> name = new ArrayList<>();
+        name.add("Samir");
+        name.add("Raja");
+        name.add("Hari");
+
+        name.add(1,"Susmita");
+
+        name.set(2,"Rajen");
+
+//        name.remove(3);
+//        name.clear();
+
+        System.out.println(name);
+//        System.out.println(name.get(0));
+//        for(int i=0; i<name.size();i++){
+//            System.out.println(name.get(i));
+//        }
+//        System.out.println();
+//        for (int i=name.size()-1; i>=0; i--){
+//            System.out.println(name.get(i));
+//        }
+
     }
 }
