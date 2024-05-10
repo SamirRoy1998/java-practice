ad42d549cde33b4da578cde713c4c26b66c628e0
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-10 22:44:36 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- My_ArrayList1.java
+++ My_ArrayList1.java
@@ -1,4 +1,28 @@
 package pkg1;
 
+import java.util.ArrayList;
+
 public class My_ArrayList1 {
+    public static void main(String[] args) {
+        ArrayList<String> batch_1 = new ArrayList<>();
+        batch_1.add("Samir");
+        batch_1.add("Raja");
+        batch_1.add("Hari");
+        batch_1.add("Gautam");
+        batch_1.add("Abhinav");
+
+        ArrayList<String> batch_2 = new ArrayList<>();
+        batch_2.add("Rupa");
+        batch_2.add("Anjali");
+        batch_2.add("Anita");
+        batch_2.add("Priya");
+        batch_2.add("Anushka");
+
+        batch_1.add(1,batch_2.get(4));
+//        batch_1.add(3,batch_2.get(4));
+        batch_2.remove(4);
+        batch_1.addAll(batch_2);
+        System.out.println(batch_1);
+//        System.out.println(batch_2);
+    }
 }
