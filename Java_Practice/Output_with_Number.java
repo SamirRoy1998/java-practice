337d5ad7f55d5dc1ef72f2641102ef89d4be892d
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-12 18:20:19 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/Output_with_Number.java
+++ Java_Practice/Output_with_Number.java
@@ -1,4 +1,16 @@
 package pkg1.Java_Practice;
 
+import java.util.Scanner;
+
 public class Output_with_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lineNumber = 1;
+        while (sc.hasNext()) {
+            String text = sc.nextLine();
+            System.out.println(lineNumber + " " + text);
+            lineNumber++;
+            if (lineNumber > 5) break;
+        }
+    }
 }
