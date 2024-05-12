18777379f24a38af822fd1d5c2d70937bbf5a162
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-12 19:39:52 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/Convert_int_to_String.java
+++ Java_Practice/Convert_int_to_String.java
@@ -1,4 +1,12 @@
 package pkg1.Java_Practice;
 
+import java.util.Scanner;
+
 public class Convert_int_to_String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String name = Integer.toString(num);
+        System.out.println(name);
+    }
 }
