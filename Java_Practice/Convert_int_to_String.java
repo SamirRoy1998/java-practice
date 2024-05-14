323b5bbacd8f7e8fbcbc05f04997734517c50b47
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-15 01:26:09 +0530

    Revert "just added"
    This reverts commit 04959c2a0b9a5ccce1ff9121edd6bbde392a591d.

--- Java_Practice/Convert_int_to_String.java
+++ Java_Practice/Convert_int_to_String.java
@@ -8,20 +8,5 @@ public class Convert_int_to_String {
         int num = sc.nextInt();
         String name = Integer.toString(num);
         System.out.println(name);
-
-        String abc= "10001";
-        int a = Integer.parseInt(abc);
-        System.out.println(a);
-        if (a == 10001){
-            while (true){
-                a++;
-                System.out.println(a);
-                break;
-            }
-        }
-
-        String abcd = "101";
-        double b =Double.parseDouble(abcd);
-        System.out.println(b);
     }
 }
--- Java_Practice/Phone_Smartphone.java
+++
@@ -1,4 +0,0 @@
-package pkg1.Java_Practice;
-
-public class Phone_Smartphone {
-}
