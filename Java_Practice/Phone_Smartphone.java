6915ae3692a661b76246d3d17683f9653e3a702b
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-15 01:39:20 +0530

    doubt
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Java_Practice/Phone_Smartphone.java
+++ Java_Practice/Phone_Smartphone.java
@@ -1,4 +1,27 @@
 package pkg1.Java_Practice;
 
+class MyPhone{
+    void ring(){
+        System.out.println("Ringing.....");
+    }
+    void call(){
+        System.out.println("Calling...");
+    }
+}
+
+class MySmartPhone extends MyPhone{
+    @Override
+    void call() {
+        System.out.println("Calling from Smartphone.");
+    }
+    void music(){
+        System.out.println("Playing Musics");
+    }
+}
 public class Phone_Smartphone {
+    public static void main(String[] args) {
+    MyPhone p = new MySmartPhone();
+    p.call();
+    p.ring();
+    }
 }
