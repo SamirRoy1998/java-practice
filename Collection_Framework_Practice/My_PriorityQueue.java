a22ea67d9aa44d299cad1c8ed77c01ad845d2c1f
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-17 02:52:35 +0530

    reverse the PriorityQueue
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Collection_Framework_Practice/My_PriorityQueue.java
+++ Collection_Framework_Practice/My_PriorityQueue.java
@@ -1,4 +1,34 @@
 package pkg1.Collection_Framework_Practice;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class My_PriorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> p1 = new PriorityQueue<>();
+        p1.offer(10);
+        p1.offer(42);
+        p1.offer(67);
+        p1.offer(11);
+        p1.offer(42);
+        p1.offer(72);
+        p1.offer(78);
+        p1.offer(84);
+        p1.offer(95);
+        p1.offer(52);
+        System.out.println(p1);
+
+        PriorityQueue<Integer> p2 = new PriorityQueue<>(Comparator.reverseOrder());
+        p2.offer(10);
+        p2.offer(42);
+        p2.offer(67);
+        p2.offer(11);
+        p2.offer(42);
+        p2.offer(72);
+        p2.offer(78);
+        p2.offer(84);
+        p2.offer(95);
+        p2.offer(52);
+        System.out.println(p2);
+    }
 }
