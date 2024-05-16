db1c22de9a6b9504d04f8acb4cb42b2dd470391d
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-16 16:41:02 +0530

    done
    Signed-off-by: SamirRoy1998 <dev.sam693@gmail.com>

--- Collection_Framework_Practice/My_LinkedList.java
+++ Collection_Framework_Practice/My_LinkedList.java
@@ -1,5 +1,30 @@
 package pkg1.Collection_Framework_Practice;
 
-public class My_LinkedList {
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Scanner;
 
+public class My_LinkedList {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.offer(10);
+        ll.offer(24);
+        ll.offer(12);
+        ll.offer(23);
+        ll.offer(53);
+        ll.offer(67);
+        ll.push(99);
+        System.out.println(ll);
+        ll.sort(Comparator.reverseOrder());
+        System.out.println(ll);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int input = sc.nextInt();
+            if (input > 0 && input <= ll.size()) {
+                System.out.println(ll.get(input - 1));
+                break;
+            } else System.out.println("Invalid input");
+        }
+    }
 }
